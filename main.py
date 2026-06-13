b1533f9dc463696a8aae8983c9e00750995bf950
Syed Junaidulla <junaidsyeds031@gmail.com>
2026-06-13 16:11:04 +0530

    Refactor headlines function to accept category input

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
 import requests
 
-def headlines() : 
-    url1 = "https://newsapi.org/v2/top-headlines?"
+def headlines():
 
-    params1 = {"q": "politics" , "apikey" : "1ff8e0e2f54c4ed1b182bc93664aff75" }
 
-    response = requests.get(url = url1 , params = params1)
+    category = input("Enter Category : \n1.)Politics\n2.)Technology\n3.)Sports\n4.)Business\n5.)Health\n6.)Entertainment\n: ").lower()
+
+    url1 = "https://newsapi.org/v2/top-headlines"
+
+    params1 = {"q": category,"apiKey": "1ff8e0e2f54c4ed1b182bc93664aff75"}
+
+    response = requests.get(url=url1, params=params1)
 
     data = response.json()
 
-    sequence = [2,3,4,7] 
+    if "articles" not in data:
+        print("Error :", data.get("message", "Unable to fetch news."))
+        return
 
-    for content in range(len(data["articles"])) : 
-        print("="*50)
-        for index,(key,value) in enumerate(data["articles"][content].items()) :
+    sequence = [2, 3, 4, 7]
+
+    news_count = 1
+
+    for content in range(min(5, len(data["articles"]))):
+
+        print(f"\nNEWS #{news_count}")
+        print("-" * 50)
+
+        for index, (key, value) in enumerate(data["articles"][content].items()):
+
             for j in range(4):
+
                 if index == sequence[j]:
-                    print(key.title(),":",value)
+
+                    if key.lower() == "title":
+                        print(f"Title       : {value}\n")
+
+                    elif key.lower() == "description":
+                        print(f"Description :\n{value}\n")
+
+                    elif key.lower() == "url":
+                        print(f"🔗 URL         :\n{value}\n")
+
+                
                     break
-        
-    
+
+        print("=" * 50)
+
+        news_count += 1
+
 headlines()
-
-
- 
